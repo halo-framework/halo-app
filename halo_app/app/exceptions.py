14eb067784f0ab9c26c33dd8d23bb13b8f8112fb
halo-framework <halo-framework@gmail.com>
2021-02-22 01:06:49 +0200

    fixed name

--- halo_app/app/exceptions.py
+++ halo_app/app/exceptions.py
@@ -49,29 +49,3 @@ class HttpFailException(AppException):
 class AppValidationException(AppException):
     pass
 
-
-class ConvertDomainExceptionHandler(AbsBaseClass):
-    message_service = None
-
-    #@todo add conversion service
-    def __init__(self, message_service=None):
-        self.message_service = message_service
-
-    def handle(self, de: DomainException) -> AppException:
-        #main_message = self.message_service.convert(de.message)
-        #detail_message = self.message_service.convert(de.detail)
-        return AppException (de.message, de, de.detail,de.data)
-
-class AppExceptionHandler(AbsBaseClass):
-
-    def __init__(self):
-        pass
-
-    def handle(self,halo_request,e:Exception,traceback):
-        # @todo check if stack needed and working
-        e.stack = traceback.format_exc()
-        logger.error(e.__str__(), extra=log_json(halo_request.context, {}, e))
-        # exc_type, exc_obj, exc_tb = sys.exc_info()
-        # fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        # logger.debug('An Exception occured in '+str(fname)+' lineno: '+str(exc_tb.tb_lineno)+' exc_type '+str(exc_type)+' '+e.message)
-        return e
--- halo_app/exceptions.py
+++ halo_app/exceptions.py
@@ -45,8 +45,6 @@ class StoreException(HaloException):
 class StoreClearException(HaloException):
     pass
 
-
-
 class SecureException(HaloException):
     pass
 
@@ -62,3 +60,59 @@ class BadSecurityTokenException(SecureException):
 class FilterValidationException(SecureException):
     pass
 
+
+class ConvertDomainExceptionHandler(AbsBaseClass):
+    message_service = None
+
+    #@todo add conversion service
+    def __init__(self, message_service=None):
+        self.message_service = message_service
+
+    def handle(self, de: DomainException) -> AppException:
+        #main_message = self.message_service.convert(de.message)
+        #detail_message = self.message_service.convert(de.detail)
+        return AppException (de.message, de, de.detail,de.data)
+
+class HaloErrorHandler(AbsBaseClass):
+
+    def __init__(self):
+        pass
+
+    def handle(self,halo_request,e:Exception,traceback):
+        # @todo check if stack needed and working
+        e.stack = traceback.format_exc()
+        logger.error(e.__str__(), extra=log_json(halo_request.context, {}, e))
+        # exc_type, exc_obj, exc_tb = sys.exc_info()
+        # fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        # logger.debug('An Exception occured in '+str(fname)+' lineno: '+str(exc_tb.tb_lineno)+' exc_type '+str(exc_type)+' '+e.message)
+        return e
+
+
+class HaloExceptionHandler(AbsBaseClass):
+
+    def __init__(self):
+        pass
+
+    def handle(self,halo_request,e:Exception,traceback):
+        # @todo check if stack needed and working
+        e.stack = traceback.format_exc()
+        logger.error(e.__str__(), extra=log_json(halo_request.context, {}, e))
+        # exc_type, exc_obj, exc_tb = sys.exc_info()
+        # fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        # logger.debug('An Exception occured in '+str(fname)+' lineno: '+str(exc_tb.tb_lineno)+' exc_type '+str(exc_type)+' '+e.message)
+        return e
+
+
+class ExceptionHandler(AbsBaseClass):
+
+    def __init__(self):
+        pass
+
+    def handle(self,halo_request,e:Exception,traceback):
+        # @todo check if stack needed and working
+        e.stack = traceback.format_exc()
+        logger.error(e.__str__(), extra=log_json(halo_request.context, {}, e))
+        # exc_type, exc_obj, exc_tb = sys.exc_info()
+        # fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        # logger.debug('An Exception occured in '+str(fname)+' lineno: '+str(exc_tb.tb_lineno)+' exc_type '+str(exc_type)+' '+e.message)
+        return e
